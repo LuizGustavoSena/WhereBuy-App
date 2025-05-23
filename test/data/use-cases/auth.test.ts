d2c2cf069db3a66bcf4a35fadf31c87204d1faf9
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-05-22 21:37:58 -0300

    test: AuthService - Should be successful to signup authentication

--- test/data/use-cases/auth.test.ts
+++ test/data/use-cases/auth.test.ts
@@ -1,7 +1,7 @@
 import { HttpStatusCode } from "@src/data/protocols/http/http-client";
 import AuthService from "@src/data/use-cases/auth";
 import { InvalidCredentialsError } from "@src/domain/errors/invalid-credentials";
-import { makeSigninRequest, makeSigninResponse } from "@test/domain/mocks/mock-auth";
+import { makeSigninRequest, makeSigninResponse, makeSignupRequest } from "@test/domain/mocks/mock-auth";
 import { describe, expect, it } from "vitest";
 import { HttpClientSpy, makeHttpStatusCodeWithoutCreated } from "../mocks/mock-http";
 
@@ -45,5 +45,18 @@ describe('AuthService', () => {
         const promise = sut.signin(makeSigninRequest());
 
         await expect(promise).rejects.toThrow(new InvalidCredentialsError());
+    });
+
+    it('Should be successful to signup authentication', async () => {
+        const { sut, httpClient } = makeSut();
+
+        httpClient.response = {
+            statusCode: HttpStatusCode.Created,
+            body: makeSigninResponse()
+        };
+
+        const response = await sut.signup(makeSignupRequest());
+
+        expect(response).toBeTruthy();
     });
 });
