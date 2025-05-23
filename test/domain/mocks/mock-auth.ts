8db84e5ab7595f3d78e4da776a858d4e3211cc34
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-05-22 21:37:46 -0300

    feat: makeSignupRequest function

--- test/domain/mocks/mock-auth.ts
+++ test/domain/mocks/mock-auth.ts
@@ -1,9 +1,15 @@
 import { faker } from "@faker-js/faker";
-import { SigninParams, SigninResponse } from "../models/auth";
+import { SigninParams, SigninResponse, SignupParams } from "@src/domain/models/auth";
 
 export const makeSigninRequest = (params?: Partial<SigninParams>): SigninParams => ({
     email: params && params.email ? params.email : faker.internet.email(),
     password: params && params.password ? params.password : faker.internet.password()
+});
+
+export const makeSignupRequest = (params?: Partial<SignupParams>): SignupParams => ({
+    email: params && params.email ? params.email : faker.internet.email(),
+    password: params && params.password ? params.password : faker.internet.password(),
+    name: params && params.name ? params.name : faker.person.fullName(),
 });
 
 export const makeSigninResponse = (params?: Partial<SigninResponse>): SigninResponse => ({
