1a2bbba0cde331fdeb93f07128875aa854246b54
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-05-29 23:25:36 -0300

    feat: update models

--- src/domain/models/shopping-list.ts
+++ src/domain/models/shopping-list.ts
@@ -13,6 +13,12 @@ export type CreateShoppingListResponse = { id: string };
 export type GetAllShoppingListResult = Omit<ShoppingList, 'userId'>[];
 export type GetByNameShoppingListResult = Omit<ShoppingList, 'userId'>[];
 
+export type UpdateShoppingListProps = {
+    id: string;
+    data: Partial<Omit<ShoppingList, 'id' | 'created' | 'userId'>>
+};
+export type UpdateShoppingListResult = Omit<ShoppingList, 'userId'>;
+
 export enum TypeAmountEnum {
     UNIT = 'unit',
     GRAMS = 'grams',
