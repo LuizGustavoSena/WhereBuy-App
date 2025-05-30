0364af8aac2fc6fc5ba164c8c118654fd55db62c
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-05-29 23:26:11 -0300

    test: makeShoppingListUpdate function

--- test/domain/mocks/mock-shopping-list.ts
+++ test/domain/mocks/mock-shopping-list.ts
@@ -1,5 +1,5 @@
 import { faker } from "@faker-js/faker";
-import { CreateShoppingListProps, ShoppingList, TypeAmountEnum } from "@src/domain/models/shopping-list";
+import { CreateShoppingListProps, ShoppingList, TypeAmountEnum, UpdateShoppingListProps } from "@src/domain/models/shopping-list";
 
 export const makeShoppingListCreate = (params?: Partial<CreateShoppingListProps>): CreateShoppingListProps => ({
     amount: params?.amount ?? Number(faker.commerce.price()),
@@ -15,3 +15,12 @@ export const makeShoppingListItem = (params?: Partial<Omit<ShoppingList, 'userId
     name: params?.name ?? faker.commerce.productName(),
     typeAmount: params?.typeAmount ?? faker.helpers.enumValue(TypeAmountEnum)
 });
+
+export const makeShoppingListUpdate = (params?: Partial<UpdateShoppingListProps>): UpdateShoppingListProps => ({
+    id: params?.id ?? faker.string.uuid(),
+    data: {
+        amount: params?.data?.amount ?? Number(faker.commerce.price()),
+        name: params?.data?.name ?? faker.commerce.productName(),
+        typeAmount: params?.data?.typeAmount ?? faker.helpers.enumValue(TypeAmountEnum)
+    }
+})
