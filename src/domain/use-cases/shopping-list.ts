6af712b68dd1d6405080498492d8d3d280594a5d
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-05-29 23:25:51 -0300

    feat: update in IShoppingList interface

--- src/domain/use-cases/shopping-list.ts
+++ src/domain/use-cases/shopping-list.ts
@@ -1,7 +1,8 @@
-import { CreateShoppingListProps, CreateShoppingListResponse, GetAllShoppingListResult, GetByNameShoppingListResult } from "../models/shopping-list";
+import { CreateShoppingListProps, CreateShoppingListResponse, GetAllShoppingListResult, GetByNameShoppingListResult, UpdateShoppingListProps, UpdateShoppingListResult } from "../models/shopping-list";
 
 export interface IShoppingList {
     create(params: CreateShoppingListProps): Promise<CreateShoppingListResponse>;
     getAll(): Promise<GetAllShoppingListResult>;
-    getByName(name: string): Promise<GetByNameShoppingListResult>
+    getByName(name: string): Promise<GetByNameShoppingListResult>;
+    update(params: UpdateShoppingListProps): Promise<UpdateShoppingListResult>
 }
