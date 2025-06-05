5b76c67e0592bfb3db2369261838813ab3af8e99
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-06-05 17:28:01 -0300

    test: LocalStorageCacheClient - Should be successful create and read item in local storage

--- test/infrastructure/cache/local-storage-cache-client.test.ts
+++ test/infrastructure/cache/local-storage-cache-client.test.ts
@@ -1,8 +1,25 @@
+/// <reference types="vitest" />
+// @vitest-environment jsdom
+import { faker } from "@faker-js/faker";
 import { LocalStorageCacheClient } from "@src/infrastructure/cache/local-storage-cache-client";
-import { describe } from "vitest";
+import { beforeEach, describe, expect, it } from "vitest";
+import { mockRequest } from "../mocks/mock-axios";
 
 const makeSut = () => new LocalStorageCacheClient();
 
 describe('LocalStorageCacheClient', () => {
+    beforeEach(() => localStorage.clear());
 
+    it('Should be successful create and read item in local storage', () => {
+        const sut = makeSut();
+
+        const request = mockRequest();
+        const keyName = faker.string.sample();
+
+        sut.create({ key: keyName, value: request });
+
+        const response = sut.readByKey(keyName);
+
+        expect(response).toEqual(request);
+    });
 });
