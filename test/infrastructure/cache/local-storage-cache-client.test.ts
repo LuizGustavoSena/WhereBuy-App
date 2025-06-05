fd46f310aa24f1da9bf4ab210abfde5915188740
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-06-05 17:16:01 -0300

    test: LocalStorageCacheClient

---
+++ test/infrastructure/cache/local-storage-cache-client.test.ts
@@ -0,0 +1,8 @@
+import { LocalStorageCacheClient } from "@src/infrastructure/cache/local-storage-cache-client";
+import { describe } from "vitest";
+
+const makeSut = () => new LocalStorageCacheClient();
+
+describe('LocalStorageCacheClient', () => {
+
+});
