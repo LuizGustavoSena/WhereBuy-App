779d1a77dd1f09bad3d7edfad1789955c82dfafc
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-06-05 17:08:52 -0300

    feat: create in LocalStorageCacheClient class

---
+++ src/infrastructure/cache/local-storage-cache-client.ts
@@ -0,0 +1,16 @@
+import { CreateCacheProps, ICacheClient } from "@src/data/protocols/cache/cache-client";
+
+export class LocalStorageCacheClient implements ICacheClient {
+    create({ key, value }: CreateCacheProps): void {
+        const body = JSON.stringify(value);
+
+        localStorage.setItem(key, body);
+    }
+
+    readByKey(key: string) {
+        throw new Error("Method not implemented.");
+    }
+    delete(): void {
+        throw new Error("Method not implemented.");
+    }
+}
