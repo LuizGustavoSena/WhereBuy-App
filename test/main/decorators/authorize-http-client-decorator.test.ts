a66443ba86f613e221ff133a8affc79f46079068
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-06-08 09:45:58 -0300

    test: AuthorizeHttpClientDecorator - Should be call GetStorage with correct value

--- test/main/decorators/authorize-http-client-decorator.test.ts
+++ test/main/decorators/authorize-http-client-decorator.test.ts
@@ -1,14 +1,14 @@
-import { ICacheClient } from "@src/data/protocols/cache/cache-client"
-import { IHttpClient } from "@src/data/protocols/http/http-client"
+import { CacheEnum } from "@src/domain/enums/cache-enum"
 import { AuthorizeHttpClientDecorator } from "@src/main/decorators/authorize-http-client-decorator"
 import { HttpClientSpy } from "@test/data/mocks/mock-http"
 import { CacheClientSpy } from "@test/data/mocks/mock-local-storage"
-import { describe } from "vitest"
+import { mockRequest } from "@test/infrastructure/mocks/mock-axios"
+import { describe, expect, it } from "vitest"
 
 type SutTypes = {
     sut: AuthorizeHttpClientDecorator
-    storageSpy: ICacheClient
-    httpClientSpy: IHttpClient
+    storageSpy: CacheClientSpy
+    httpClientSpy: HttpClientSpy
 }
 
 const makeSut = (): SutTypes => {
@@ -23,5 +23,11 @@ const makeSut = (): SutTypes => {
 }
 
 describe('AuthorizeHttpClientDecorator', () => {
+    it('Should be call GetStorage with correct value', async () => {
+        const { sut, storageSpy } = makeSut();
 
+        await sut.request(mockRequest());
+
+        expect(storageSpy.params.key).toBe(CacheEnum.AUTH_CACHE);
+    })
 });
