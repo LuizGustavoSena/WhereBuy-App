2fe9d5c7188ec5c49cf6cc7d5d8988439b96c695
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-06-12 14:26:20 -0300

    feat: adjust errors tailwind

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,14 @@
 import { Stack } from 'expo-router';
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import './global.css';
 
 export default function Layout() {
     return (
-        <Stack>
-            <Stack.Screen name="login" options={{ headerShown: false }} />
-        </Stack>
+        <SafeAreaView className='flex-1'>
+            <Stack>
+                <Stack.Screen name="login" options={{ headerShown: false }} />
+            </Stack>
+        </SafeAreaView>
     );
 }
