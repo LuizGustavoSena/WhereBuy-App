1ebabd1c21d856b5f25ff294562fb270e17e98a8
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-06-16 18:23:24 -0300

    feat: initialize gluestack

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,3 +1,4 @@
+import { GluestackUIProvider } from '@src/components/ui/gluestack-ui-provider';
 import { Stack } from 'expo-router';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -5,10 +6,12 @@ import './global.css';
 
 export default function Layout() {
     return (
-        <SafeAreaView className='flex-1'>
-            <Stack>
-                <Stack.Screen name="login" options={{ headerShown: false }} />
-            </Stack>
-        </SafeAreaView>
+        <GluestackUIProvider>
+            <SafeAreaView className='flex-1'>
+                <Stack>
+                    <Stack.Screen name="login" options={{ headerShown: false }} />
+                </Stack>
+            </SafeAreaView>
+        </GluestackUIProvider>
     );
 }
