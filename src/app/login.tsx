bc0614bfc91a3c187eb3754df6b66658fefcf21f
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-06-16 18:23:43 -0300

    feat: login struct

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Login() {
     return (
         <>
-            <View className="flex-1 bg-red-500">
-
+            <View className="w-full bg-blue-500" style={{ flex: 1 }}>
             </View>
-            <View className='flex-1 bg-blue-500'>
 
-                {/* <Button title='Logar' action={() => alert('apertou')} /> */}
+            <View className="w-full" style={{ flex: 2 }}>
+                <Text className='text-center text-2xl'>Bem vindo de volta</Text>
             </View>
         </>
     );
