edf4b241609736efb620f57a3330876db00f04d6
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-06-19 17:00:32 -0300

    fix: adjust env names

--- src/infrastructure/env/index.ts
+++ src/infrastructure/env/index.ts
@@ -2,8 +2,8 @@ import { InvalidEnvError } from "@src/domain/errors/invalid-env";
 import { z } from "zod";
 
 const envSchema = z.object({
-    URL_AUTH: z.string(),
-    URL_SHOPPING_LIST: z.string()
+    EXPO_PUBLIC_URL_AUTH: z.string(),
+    EXPO_PUBLIC_URL_SHOPPING_LIST: z.string()
 });
 
 const _env = envSchema.safeParse(process.env);
