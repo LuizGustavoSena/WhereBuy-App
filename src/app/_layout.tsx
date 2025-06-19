110c9a1e955988e937c653daa61c30ad441e1fb2
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-06-19 00:03:09 -0300

    feat: remove gluestack

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,4 +1,3 @@
-import { GluestackUIProvider } from '@src/components/ui/gluestack-ui-provider';
 import { Stack } from 'expo-router';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -6,12 +5,10 @@ import './global.css';
 
 export default function Layout() {
     return (
-        <GluestackUIProvider>
-            <SafeAreaView className='flex-1'>
-                <Stack>
-                    <Stack.Screen name="login" options={{ headerShown: false }} />
-                </Stack>
-            </SafeAreaView>
-        </GluestackUIProvider>
+        <SafeAreaView className='flex-1'>
+            <Stack>
+                <Stack.Screen name="login" options={{ headerShown: false }} />
+            </Stack>
+        </SafeAreaView>
     );
 }
