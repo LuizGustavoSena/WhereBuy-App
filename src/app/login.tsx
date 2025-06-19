0ef6bb2ae9586f73f9978c12fd824d85793f3574
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-06-19 16:49:26 -0300

    feat: login footer

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -1,6 +1,7 @@
+import Button from '@src/components/button';
 import Input from '@src/components/input';
 import React, { useState } from 'react';
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 
 export default function Login() {
     const [email, setEmail] = useState<string | null>();
@@ -17,6 +18,13 @@ export default function Login() {
                 <View className='p-4'>
                     <Input placeholder='Email' action={(text) => setEmail(text)} />
                     <Input placeholder='Senha' action={(text) => setPass(text)} secureTextEntry />
+
+                    <View className='flex flex-row justify-around m-5'>
+                        <Pressable className='justify-center' onPress={() => { }}>
+                            <Text className='text-lg color-blue-400'>Não possui cadastro?</Text>
+                        </Pressable>
+                        <Button title='Entrar' action={() => { }} />
+                    </View>
                 </View>
             </View>
         </>
