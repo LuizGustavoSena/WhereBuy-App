5aeda493ea3a15300513f0a98042e09f94232064
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-06-22 10:18:36 -0300

    feat: title modal error

--- src/components/modals/modal-error/index.tsx
+++ src/components/modals/modal-error/index.tsx
@@ -11,8 +11,9 @@ export default function ModalError({ show, message, setModal }: ModalErrorProps)
     return (
         <Modal isVisible={show}>
             <View className="flex justify-center items-center">
-                <View className="flex justify-evenly items-center bg-white rounded-lg items-center w-[250px] h-[200px] border-t-[7px] border-purple-300">
+                <View className="flex justify-evenly items-center bg-white rounded-lg items-center w-[300px] h-[250px] border-t-[7px] border-purple-300">
                     <Image source={require('../../../../assets/error.png')} />
+                    <Text className="text-lg font-bold">Erro ao executar ação!</Text>
                     <Text>{message}</Text>
                     <Button title="Entendi" action={() => setModal(null)} />
                 </View>
