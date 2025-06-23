90e9e4d8d2c1218fb0822a1f5b536bcd676f51f6
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-06-23 17:12:29 -0300

    test: AuthValidationZod - Should be successfull login with correct params

--- test/infrastructure/validations/auth-validation-zod.test.ts
+++ test/infrastructure/validations/auth-validation-zod.test.ts
@@ -1,8 +1,11 @@
-import { describe } from "vitest";
-import { AuthValidationZod } from "./auth-validation-zod";
+import { AuthValidationZod } from "@src/infrastructure/validations/auth-validation-zod";
+import { makeSigninRequest } from "@test/domain/mocks/mock-auth";
+import { describe, expect, it } from "vitest";
 
 export const sut = new AuthValidationZod();
 
 describe('AuthValidationZod', () => {
-
+    it('Should be successfull login with correct params', () => {
+        expect(sut.login(makeSigninRequest())).toBeUndefined();
+    });
 });
