d5f0e8777efe18d4a2d9950a74f30fb4a5407a0a
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-06-23 17:24:18 -0300

    test: AuthValidationZod - Should be error login with incorrect email

--- test/infrastructure/validations/auth-validation-zod.test.ts
+++ test/infrastructure/validations/auth-validation-zod.test.ts
@@ -1,3 +1,6 @@
+import { faker } from "@faker-js/faker";
+import { InvalidCredentialsError } from '@src/domain/errors/invalid-credentials';
+import { AuthMessageType } from "@src/domain/validations/auth-validation";
 import { AuthValidationZod } from "@src/infrastructure/validations/auth-validation-zod";
 import { makeSigninRequest } from "@test/domain/mocks/mock-auth";
 import { describe, expect, it } from "vitest";
@@ -7,5 +10,14 @@ export const sut = new AuthValidationZod();
 describe('AuthValidationZod', () => {
     it('Should be successfull login with correct params', () => {
         expect(sut.login(makeSigninRequest())).toBeUndefined();
+    });
+
+    it('Should be error login with incorrect email', () => {
+        const request = {
+            email: faker.number.int(),
+            password: faker.string.sample()
+        };
+        // @ts-expect-error
+        expect(() => sut.login(request)).toThrow(new InvalidCredentialsError(AuthMessageType.EMAIL));
     });
 });
