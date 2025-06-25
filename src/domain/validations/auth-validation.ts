47d961788c9cf49f58d9db6d2013d8bd093f8b22
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-06-25 17:11:52 -0300

    feat: signup method in IAuthValidation

--- src/domain/validations/auth-validation.ts
+++ src/domain/validations/auth-validation.ts
@@ -1,14 +1,17 @@
-import { SigninParams } from "../models/auth";
+import { SigninParams, SignupParams } from "../models/auth";
 
 export enum AuthMessageRequire {
     EMAIL = 'O campo email deve ser preenchido',
     PASSWORD = 'O campo password deve ser preenchido',
+    NAME = 'O campo name deve ser preenchido',
 }
 
 export enum AuthMessageType {
     EMAIL = 'O campo email deve ser do tipo string',
     PASSWORD = 'O campo password deve ser do tipo string',
+    NAME = 'O campo name deve ser do tipo string',
 }
 export interface IAuthValidation {
     signin(params: SigninParams): void | Error;
+    signup(params: SignupParams): void | Error;
 }
