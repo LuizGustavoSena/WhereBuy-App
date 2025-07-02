62a60766c09b08477b1e236c1bbbba4ef5a997d1
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-07-02 17:25:37 -0300

    feat: add className property from Button component

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -3,10 +3,11 @@ import { Pressable, Text } from 'react-native';
 type Props = {
     title: string;
     action: Function;
+    className?: string;
 }
-export default function Button({ title, action }: Props) {
+export default function Button({ title, action, className }: Props) {
     return (
-        <Pressable className='bg-purple-300 p-3 rounded-lg' onPress={() => action()}>
+        <Pressable className={`bg-purple-300 p-3 rounded-lg ${className}`} onPress={() => action()}>
             <Text className='text-lg'>{title}</Text>
         </Pressable>
     );
