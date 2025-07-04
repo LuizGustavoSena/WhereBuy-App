9b7f4bccd7c563d9338711ae150b503d14f56686
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-07-04 17:32:10 -0300

    feat: ShoppingListValidationZod class

---
+++ src/infrastructure/validations/shopping-list-validation-zod.ts
@@ -0,0 +1,46 @@
+import { InvalidCredentialsError } from "@src/domain/errors/invalid-credentials";
+import { CreateShoppingListProps, TypeAmountEnum } from "@src/domain/models/shopping-list";
+import { IShoppingListValidation, ShoppingListMessageRequire, ShoppingListMessageType } from "@src/domain/validations/shopping-list-validation";
+import { z, ZodError } from "zod";
+
+export class ShoppingListValidationZod implements IShoppingListValidation {
+    idSchema = z.string({
+        required_error: ShoppingListMessageRequire.ID,
+        invalid_type_error: ShoppingListMessageType.ID
+    }).uuid({ message: ShoppingListMessageType.ID });
+
+    nameSchema = z.string({
+        required_error: ShoppingListMessageRequire.NAME,
+        invalid_type_error: ShoppingListMessageType.NAME
+    });
+
+    amountSchema = z.number({
+        required_error: ShoppingListMessageRequire.AMOUNT,
+        invalid_type_error: ShoppingListMessageType.AMOUNT
+    });
+
+    typeAmountSchema = z.nativeEnum(TypeAmountEnum, {
+        required_error: ShoppingListMessageRequire.TYPE_AMOUNT,
+        invalid_type_error: ShoppingListMessageType.TYPE_AMOUNT
+    });
+
+    create(params: CreateShoppingListProps): void | Error {
+        const validation = z.object({
+            name: this.nameSchema,
+            amount: this.amountSchema,
+            typeAmount: this.typeAmountSchema
+        });
+
+        this.throwValidationError(() => validation.parse(params));
+    }
+
+    private throwValidationError(callback: Function) {
+        try {
+            callback();
+        } catch (error: any) {
+            if (!(error instanceof ZodError)) return;
+
+            throw new InvalidCredentialsError(error.errors.map(el => el.message).join(', '));
+        }
+    }
+}
