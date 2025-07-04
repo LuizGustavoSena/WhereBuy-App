19323032685d61b8a08e522d2d05cfdc5e9c0422
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-07-04 17:32:01 -0300

    feat: IShoppingListValidation interface

---
+++ src/domain/validations/shopping-list-validation.ts
@@ -0,0 +1,19 @@
+import { CreateShoppingListProps, TypeAmountEnum } from "../models/shopping-list";
+
+export enum ShoppingListMessageRequire {
+    ID = 'O id deve ser preenchido',
+    NAME = 'O nome deve ser preenchido',
+    AMOUNT = 'A quantidade deve ser preenchido',
+    TYPE_AMOUNT = 'O tipo de quantidade deve ser preenchido'
+}
+
+export const ShoppingListMessageType = {
+    ID: 'Id deve ser do tipo guid',
+    NAME: 'Nome deve ser do tipo string',
+    AMOUNT: 'Quantidade deve ser do tipo number',
+    TYPE_AMOUNT: `Tipo de quantidade não reconhecido, informe: ${Object.values(TypeAmountEnum).join(', ')}`
+};
+
+export interface IShoppingListValidation {
+    create(params: CreateShoppingListProps): void | Error;
+}
