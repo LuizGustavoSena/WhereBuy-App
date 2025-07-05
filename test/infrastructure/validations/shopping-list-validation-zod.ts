c12e0e956b72adc50d12c536e737f903616bfc62
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-07-05 14:20:54 -0300

    test: ShoppingListValidationZod

---
+++ test/infrastructure/validations/shopping-list-validation-zod.ts
@@ -0,0 +1,10 @@
+import { ShoppingListValidationZod } from "@src/infrastructure/validations/shopping-list-validation-zod";
+import { describe, it } from "vitest";
+
+const sut = new ShoppingListValidationZod();
+
+describe('ShoppingListValidationZod', () => {
+    it('', () => {
+
+    });
+});
