266a63589624feaa206ef289f5abb0fca7f70999
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-07-05 14:34:24 -0300

    test: ShoppingListValidationZod - Should be error when create shopping list item with incorrect name type

--- test/infrastructure/validations/shopping-list-validation-zod.test.ts
+++ test/infrastructure/validations/shopping-list-validation-zod.test.ts
@@ -1,3 +1,7 @@
+import { faker } from "@faker-js/faker";
+import { InvalidCredentialsError } from "@src/domain/errors/invalid-credentials";
+import { TypeAmountEnum } from "@src/domain/models/shopping-list";
+import { ShoppingListMessageType } from "@src/domain/validations/shopping-list-validation";
 import { ShoppingListValidationZod } from "@src/infrastructure/validations/shopping-list-validation-zod";
 import { makeShoppingListCreate } from "@test/domain/mocks/mock-shopping-list";
 import { describe, expect, it } from "vitest";
@@ -7,5 +11,16 @@ const sut = new ShoppingListValidationZod();
 describe('ShoppingListValidationZod', () => {
     it('Should be successful create shopping list item', () => {
         expect(sut.create(makeShoppingListCreate())).toBeUndefined();
+    });
+
+    it('Should be error when create shopping list item with incorrect name type', () => {
+
+        const params = {
+            typeAmount: faker.helpers.enumValue(TypeAmountEnum),
+            name: faker.number.int(),
+            amount: faker.number.int()
+        };
+        // @ts-expect-error
+        expect(() => sut.create(params)).toThrow(new InvalidCredentialsError(ShoppingListMessageType.NAME));
     });
 });
