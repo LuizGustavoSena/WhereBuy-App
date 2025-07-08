060995ea837c18ffdb104c8463aaed86d42d9b87
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-07-08 19:55:11 -0300

    feat: errorMessage property in Input component

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,4 +1,4 @@
-import { TextInput } from "react-native";
+import { Text, TextInput } from "react-native";
 
 type Props = {
     placeholder: string;
@@ -6,16 +6,20 @@ type Props = {
     value?: string;
     className?: string;
     secureTextEntry?: boolean;
+    errorMessage?: string;
 }
 
-export default function Input({ placeholder, action, value, className, secureTextEntry }: Props) {
+export default function Input({ placeholder, action, value, className, secureTextEntry, errorMessage }: Props) {
     return (
-        <TextInput
-            className={`border border-purple-800 rounded-md p-4 mt-5 ${className}`}
-            onChangeText={(text) => action(text)}
-            value={value}
-            placeholder={placeholder}
-            secureTextEntry={secureTextEntry}
-        />
+        <>
+            <TextInput
+                className={`border border-purple-800 rounded-md p-4 mt-5 ${className} ${errorMessage && 'border-red-400'}`}
+                onChangeText={(text) => action(text)}
+                value={value}
+                placeholder={placeholder}
+                secureTextEntry={secureTextEntry}
+            />
+            <Text>{errorMessage}</Text>
+        </>
     )
 }
