b130cf0cd2d81bb961fd37d2aa771f04c68f9c98
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-07-08 19:54:55 -0300

    fix: adjust response type function

--- src/main/fatories/shopping-list-validation.ts
+++ src/main/fatories/shopping-list-validation.ts
@@ -1,4 +1,3 @@
-import { IShoppingListValidation } from "@src/domain/validations/shopping-list-validation";
 import { ShoppingListValidationZod } from "@src/infrastructure/validations/shopping-list-validation-zod";
 
-export const makeShoppingListValidation = (): IShoppingListValidation => new ShoppingListValidationZod();
+export const makeShoppingListValidation = (): ShoppingListValidationZod => new ShoppingListValidationZod();
