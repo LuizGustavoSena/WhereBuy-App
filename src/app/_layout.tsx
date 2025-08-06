9380677cec059bffcecd7274a041088a230df57d
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-08-06 18:22:13 -0300

    feat: layout config to drawer route

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,15 +1,23 @@
-import { Stack } from 'expo-router';
+import { Drawer } from 'expo-router/drawer';
 import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import './global.css';
 
 export default function Layout() {
     return (
         <SafeAreaView className='flex-1'>
-            <Stack>
-                <Stack.Screen name="login" options={{ headerShown: false }} />
-                <Stack.Screen name="shopping-list" options={{ headerShown: false }} />
-            </Stack>
+            <GestureHandlerRootView style={{ flex: 1 }}>
+                <Drawer>
+                    <Drawer.Screen
+                        name="options"
+                        options={{
+                            drawerLabel: 'Configurações',
+                            title: 'Configurações',
+                        }}
+                    />
+                </Drawer>
+            </GestureHandlerRootView>
         </SafeAreaView>
     );
 }
