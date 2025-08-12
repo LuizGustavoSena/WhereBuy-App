ca00abc2d914de7034837de73cc5d081c33d06ce
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-08-12 19:46:13 -0300

    feat: back on the other page button in options page

--- src/app/options.tsx
+++ src/app/options.tsx
@@ -1,7 +1,17 @@
-import { Text } from "react-native";
+import { useRouter } from "expo-router";
+import { Image, Pressable, Text, View } from "react-native";
 
 export default function Options() {
+    const router = useRouter();
+
     return (
-        <Text>Hello Options</Text>
+        <View className='w-full h-full'>
+            <View className="flex flex-row w-full p-2 bg-gray-200">
+                <Pressable className="flex flex-row" onPress={() => router.back()}>
+                    <Image className="w-[30px] h-[30px] ml-[5px]" source={require('../../assets/left-arrow-icon.png')} />
+                    <Text className="ml-3 text-[20px]">Voltar</Text>
+                </Pressable>
+            </View>
+        </View>
     )
 }
