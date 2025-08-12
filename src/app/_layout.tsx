2fb48a21102f92d7b39f48ca3d00eedd744622a4
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-08-12 19:46:45 -0300

    feat: options page on layout config

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,6 @@
 import { Stack } from 'expo-router';
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import './global.css';
 
 export default function Layout() {
@@ -9,6 +9,7 @@ export default function Layout() {
             <Stack>
                 <Stack.Screen name="login" options={{ headerShown: false }} />
                 <Stack.Screen name="shopping-list" options={{ headerShown: false }} />
+                <Stack.Screen name="options" options={{ headerShown: false }} />
             </Stack>
         </SafeAreaView>
     );
