4c8c97fc516a2ee9c34e8c5c82c33e7a9b657f31
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-08-18 20:06:50 -0300

    feat: cache loader to signin function

--- src/data/use-cases/auth.ts
+++ src/data/use-cases/auth.ts
@@ -1,12 +1,15 @@
+import { CacheEnum } from "@src/domain/enums/cache-enum";
 import { ServerError } from "@src/domain/errors/server-error";
 import { SigninParams, SigninResponse, SignupParams } from "@src/domain/models/auth";
 import { IAuth } from "@src/domain/use-cases/auth";
 import { env } from "@src/infrastructure/env";
+import { ICacheClient } from "../protocols/cache/cache-client";
 import { HttpStatusCode, IHttpClient } from "../protocols/http/http-client";
 
 export default class AuthUseCase implements IAuth {
     constructor(
-        private httpClient: IHttpClient
+        private httpClient: IHttpClient,
+        private readonly cacheStorage: ICacheClient,
     ) { };
 
     signin = async (params: SigninParams): Promise<SigninResponse> => {
@@ -16,10 +19,22 @@ export default class AuthUseCase implements IAuth {
             body: params
         });
 
-        if (response.statusCode === HttpStatusCode.Created)
-            return { token: response.body.token };
+        if (response.statusCode !== HttpStatusCode.Created)
+            throw new ServerError(response.body.message);
 
-        throw new ServerError(response.body.message);
+        this.cacheStorage.create({
+            key: CacheEnum.AUTH_CACHE,
+            value: {
+                token: response.body.token,
+                refreshToken: response.body.refreshToken
+            }
+        });
+
+        return {
+            token: response.body.token,
+            refreshToken: response.body.refreshToken
+        };
+
     }
 
     signup = async (params: SignupParams): Promise<void> => {
