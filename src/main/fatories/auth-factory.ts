b2fa5a1df4fd6010a66bf6ed933dc9bf57a3c50f
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-08-20 23:03:13 -0300

    feat: add local storage on makeAuth

--- src/main/fatories/auth-factory.ts
+++ src/main/fatories/auth-factory.ts
@@ -1,4 +1,5 @@
 import AuthUseCase from "@src/data/use-cases/auth";
 import { makeHttpClient } from "./http-client-factory";
+import { makeLocalStorageCacheClient } from "./local-storage-cache-client-factory";
 
-export const makeAuth = (): AuthUseCase => new AuthUseCase(makeHttpClient())
+export const makeAuth = (): AuthUseCase => new AuthUseCase(makeHttpClient(), makeLocalStorageCacheClient());
