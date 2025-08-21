9c773537b59ad8822a715ed5aaf03d021b0ee286
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-08-20 23:02:58 -0300

    feat: refresh token in AuthorizeHttpClientDecorator

--- src/main/decorators/authorize-http-client-decorator.ts
+++ src/main/decorators/authorize-http-client-decorator.ts
@@ -1,7 +1,7 @@
 import { ICacheClient } from "@src/data/protocols/cache/cache-client";
-import { HttpRequest, HttpResponse, IHttpClient } from "@src/data/protocols/http/http-client";
+import { HttpRequest, HttpResponse, HttpStatusCode, IHttpClient } from "@src/data/protocols/http/http-client";
 import { CacheEnum } from "@src/domain/enums/cache-enum";
-
+import { env } from "@src/infrastructure/env";
 
 export class AuthorizeHttpClientDecorator implements IHttpClient {
     constructor(
@@ -10,17 +10,41 @@ export class AuthorizeHttpClientDecorator implements IHttpClient {
     ) { }
 
     async request(data: HttpRequest): Promise<HttpResponse<any>> {
-        const accessToken = this.cacheStorage.readByKey(CacheEnum.AUTH_CACHE);
+        var httpResponse;
+        var count = 0;
 
-        if (accessToken) {
-            Object.assign(data, {
-                headers: Object.assign(data.headers || {}, {
-                    'Authorization': accessToken
+        do {
+            count++;
+
+            const accessToken = this.cacheStorage.readByKey(CacheEnum.AUTH_CACHE);
+
+            if (accessToken && accessToken.token) {
+                Object.assign(data, {
+                    headers: Object.assign(data.headers || {}, {
+                        'Authorization': accessToken.token
+                    })
                 })
-            })
-        }
+            }
 
-        const httpResponse = await this.httpClient.request(data);
+            httpResponse = await this.httpClient.request(data);
+
+            if (httpResponse.statusCode === HttpStatusCode.Unauthorized) {
+                const response = await this.httpClient.request({
+                    method: 'post',
+                    url: `${env.EXPO_PUBLIC_URL_AUTH}/refresh-token`,
+                    headers: { refresh_token: accessToken.refreshToken }
+                });
+
+                this.cacheStorage.create({
+                    key: CacheEnum.AUTH_CACHE,
+                    value: {
+                        token: response.body.token,
+                        refreshToken: response.body.refreshToken
+                    }
+                });
+            }
+        } while (httpResponse.statusCode !== HttpStatusCode.Unauthorized || count === 3);
+
 
         return httpResponse;
     }
